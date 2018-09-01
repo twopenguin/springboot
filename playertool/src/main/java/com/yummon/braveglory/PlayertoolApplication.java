85a63f908d812b799f66ae3425f67bfc07c30031
twopenguin <1289410799@qq.com>
2018-09-01 17:36:35 +0800

    提交人：zcd review：first commit！

---
+++ playertool/src/main/java/com/yummon/braveglory/PlayertoolApplication.java
@@ -0,0 +1,12 @@
+package com.yummon.braveglory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayertoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayertoolApplication.class, args);
+	}
+}
